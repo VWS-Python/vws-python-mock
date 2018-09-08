565a1c3767b2efec08ea51894a75169ad1cb20cf
Adam Dangoor <adamdangoor@gmail.com>
2018-09-08 18:01:14 +0100

    Try to fix versioneer

--- src/mock_vws/__init__.py
+++ src/mock_vws/__init__.py
@@ -188,3 +188,7 @@ class MockVWS(ContextDecorator):
 
 __version__ = get_versions()['version']  # type: ignore
 del get_versions
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
