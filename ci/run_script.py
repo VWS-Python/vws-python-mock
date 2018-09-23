afdcf0f408674b3a9b9ceae59e01c9ee6574944b
Adam Dangoor <adamdangoor@gmail.com>
2018-09-23 12:48:59 +0100

    Undo unnecessary change

--- ci/run_script.py
+++ ci/run_script.py
@@ -20,10 +20,8 @@ def run_test(ci_pattern: str) -> None:
             '-vvv',
             '--exitfirst',
             str(path),
-            '--cov'
-            'src',
-            '--cov'
-            'tests',
+            '--cov=src',
+            '--cov=tests',
         ],
     )
     sys.exit(result)
