466bc0e4401164c3b8783507ff9b07e60bfc5b34
Adam Dangoor <adamdangoor@gmail.com>
2018-09-25 00:17:01 +0100

    Remove pdbs

--- src/mock_vws/_mock_common.py
+++ src/mock_vws/_mock_common.py
@@ -187,8 +187,4 @@ def get_database_matching_client_keys(
         )
 
         if auth_header == expected_authorization_header:
-            import pdb; pdb.set_trace()
             return database
-
-    import pdb; pdb.set_trace()
-    pass
--- src/mock_vws/_mock_web_query_api.py
+++ src/mock_vws/_mock_web_query_api.py
@@ -82,6 +82,7 @@ def route(
         )
 
         decorators = [
+            validate_authorization,
             validate_date_in_range,
             validate_date_format,
             validate_date_header_given,
@@ -94,9 +95,8 @@ def route(
             validate_extra_fields,
             validate_content_type_header,
             validate_accept_header,
-            validate_project_state,
-            validate_authorization,
             validate_auth_header_exists,
+            validate_project_state,
             set_content_length_header,
         ]
 
