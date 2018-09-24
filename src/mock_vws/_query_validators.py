890925c9530a7633b32a39c1bc39cb9542eac010
Adam Dangoor <adamdangoor@gmail.com>
2018-09-24 12:29:57 +0100

    Move query auth header check to query validators [skip ci]

--- src/mock_vws/_query_validators.py
+++ src/mock_vws/_query_validators.py
@@ -25,6 +25,38 @@ from mock_vws._mock_common import (
 
 
 @wrapt.decorator
+def validate_auth_header_exists(
+    wrapped: Callable[..., str],
+    instance: Any,  # pylint: disable=unused-argument
+    args: Tuple[_RequestObjectProxy, _Context],
+    kwargs: Dict,
+) -> str:
+    """
+    Validate that there is an authorization header given to the query endpoint.
+
+    Args:
+        wrapped: An endpoint function for `requests_mock`.
+        instance: The class that the endpoint function is in.
+        args: The arguments given to the endpoint function.
+        kwargs: The keyword arguments given to the endpoint function.
+
+    Returns:
+        The result of calling the endpoint.
+        An `UNAUTHORIZED` response if there is no "Authorization" header.
+    """
+    request, context = args
+    if 'Authorization' in request.headers:
+        return wrapped(*args, **kwargs)
+
+    context.status_code = codes.UNAUTHORIZED
+    text = 'Authorization header missing.'
+    content_type = 'text/plain; charset=ISO-8859-1'
+    context.headers['Content-Type'] = content_type
+    context.headers['WWW-Authenticate'] = 'VWS'
+    return text
+
+
+@wrapt.decorator
 def validate_authorization(
     wrapped: Callable[..., str],
     instance: Any,
