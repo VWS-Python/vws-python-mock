20451a43d3caa06549207e29826f4a509329ea3c
Adam Dangoor <adamdangoor@gmail.com>
2018-09-29 12:22:41 +0100

    Fix target import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,8 @@ sys.path.insert(0, os.path.abspath('.'))
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
     'sphinxcontrib.spelling',
 ]
 
