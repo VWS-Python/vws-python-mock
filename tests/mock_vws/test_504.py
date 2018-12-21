d1bfcbd5c7a36b56584fb836778118606d2916a4
Adam Dangoor <adamdangoor@gmail.com>
2018-12-21 14:37:10 +0000

    Progress towards test

--- tests/mock_vws/test_504.py
+++ tests/mock_vws/test_504.py
@@ -3,9 +3,9 @@ from datetime import datetime, timedelta
 import pytest
 import requests
 from requests import codes
+from requests.structures import CaseInsensitiveDict
 
 from mock_vws._constants import ResultCodes
-from mock_vws.database import VuforiaDatabase
 from tests.mock_vws.utils import Endpoint
 from tests.mock_vws.utils.assertions import assert_vws_failure
 from tests.mock_vws.utils.authorization import (
@@ -16,13 +16,9 @@ from tests.mock_vws.utils.authorization import (
 
 @pytest.mark.usefixtures('verify_mock_vuforia')
 class Test504:
-    def test_invalid_json(
-        self,
-        vuforia_database: VuforiaDatabase,
-        endpoint: Endpoint,
-    ) -> None:
+    def test_example(self, endpoint: Endpoint) -> None:
         """
-        Giving invalid JSON to endpoints returns error responses.
+        XXX
         """
         content = b'a'
         date = rfc_1123_date()
@@ -35,11 +31,11 @@ class Test504:
         endpoint_headers = dict(endpoint.prepared_request.headers)
 
         authorization_string = authorization_header(
-            access_key=vuforia_database.server_access_key,
-            secret_key=vuforia_database.server_secret_key,
+            access_key=endpoint.access_key,
+            secret_key=endpoint.secret_key,
             method=str(endpoint.prepared_request.method),
             content=content,
-            content_type=content_type,
+            content_type=endpoint.auth_header_content_type,
             date=date,
             request_path=endpoint.prepared_request.path_url,
         )
@@ -57,9 +53,7 @@ class Test504:
             files=None,
         )
 
-        endpoint.prepared_request.prepare_headers(  # type: ignore
-            headers=headers,
-        )
+        endpoint.prepared_request.headers = CaseInsensitiveDict(data=headers)
         session = requests.Session()
         response = session.send(  # type: ignore
             request=endpoint.prepared_request,
