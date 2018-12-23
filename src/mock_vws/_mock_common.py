d6ea3ed08f17d855a1f5e278de5210afe96b7799
Adam Dangoor <adamdangoor@gmail.com>
2018-12-23 10:31:24 +0000

    Make fix-lint more effective

--- src/mock_vws/_mock_common.py
+++ src/mock_vws/_mock_common.py
@@ -2,10 +2,7 @@
 Common utilities for creating mock routes.
 """
 
-import base64
 import cgi
-import hashlib
-import hmac
 import io
 import json
 from typing import (
@@ -117,64 +114,6 @@ def parse_multipart(  # pylint: disable=invalid-name
     }
 
     return cgi.parse_multipart(fp=fp, pdict=pdict)
-
-
-def _compute_hmac_base64(key: bytes, data: bytes) -> bytes:
-    """
-    Return the Base64 encoded HMAC-SHA1 hash of the given `data` using the
-    provided `key`.
-    """
-    hashed = hmac.new(key=key, msg=None, digestmod=hashlib.sha1)
-    hashed.update(msg=data)
-    return base64.b64encode(s=hashed.digest())
-
-
-def _authorization_header(  # pylint: disable=too-many-arguments
-    access_key: str,
-    secret_key: str,
-    method: str,
-    content: bytes,
-    content_type: str,
-    date: str,
-    request_path: str,
-) -> bytes:
-    """
-    Return an `Authorization` header which can be used for a request made to
-    the VWS API with the given attributes.
-
-    Args:
-        access_key: A VWS server or client access key.
-        secret_key: A VWS server or client secret key.
-        method: The HTTP method which will be used in the request.
-        content: The request body which will be used in the request.
-        content_type: The `Content-Type` header which will be used in the
-            request.
-        date: The current date which must exactly match the date sent in the
-            `Date` header.
-        request_path: The path to the endpoint which will be used in the
-            request.
-    """
-    hashed = hashlib.md5()
-    hashed.update(content)
-    content_md5_hex = hashed.hexdigest()
-
-    components_to_sign = [
-        method,
-        content_md5_hex,
-        content_type,
-        date,
-        request_path,
-    ]
-    string_to_sign = '\n'.join(components_to_sign)
-    signature = _compute_hmac_base64(
-        key=secret_key.encode(),
-        data=bytes(
-            string_to_sign,
-            encoding='utf-8',
-        ),
-    )
-    auth_header = b'VWS %s:%s' % (access_key.encode(), signature)
-    return auth_header
 
 
 def get_database_matching_client_keys(
