8c78b0ccb9bfd7b7db15e241d9fee4e57fde7035
Adam Dangoor <adamdangoor@gmail.com>
2019-12-06 23:34:37 +0000

    Move a bunch of options from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -14,33 +14,9 @@ with open('requirements.txt') as requirements:
 with open('dev-requirements.txt') as dev_requirements:
     DEV_REQUIRES = dev_requirements.readlines()
 
-with open('README.rst') as f:
-    LONG_DESCRIPTION = f.read()
-
 setup(
-    name='VWS Python Mock',
     version=versioneer.get_version(),  # type: ignore
     cmdclass=versioneer.get_cmdclass(),  # type: ignore
-    author='Adam Dangoor',
-    author_email='adamdangoor@gmail.com',
-    description='A mock for the Vuforia Web Services (VWS) API.',
-    long_description=LONG_DESCRIPTION,
-    license='MIT',
-    packages=find_packages(where='src'),
-    zip_safe=False,
-    url='https://vws-python-mock.readthedocs.io',
-    keywords='vuforia mock fake client',
-    package_dir={'': 'src'},
     install_requires=INSTALL_REQUIRES,
-    extras_require={
-        'dev': DEV_REQUIRES,
-    },
-    include_package_data=True,
-    classifiers=[
-        'Operating System :: POSIX',
-        'Environment :: Web Environment',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-        'Development Status :: 5 - Production/Stable',
-    ],
+    extras_require={'dev': DEV_REQUIRES},
 )
