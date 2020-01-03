ae7dbd714f6f85c24492955c22c6f433404ed4ca
Adam Dangoor <adamdangoor@gmail.com>
2020-01-03 20:51:07 +0000

    Fix some lint issues

--- src/mock_vws/_base64_decoding.py
+++ src/mock_vws/_base64_decoding.py
@@ -1,9 +1,20 @@
+"""
+Helpers for handling Base64 like Vuforia does.
+"""
+
 import base64
 import binascii
 import string
 
 
-def decode_base64(encoded_data):
+def decode_base64(encoded_data: str) -> bytes:
+    """
+    Decode base64 somewhat like Vuforia does.
+
+    Raises:
+        binascii.Error: Vuforia would consider this encoded data as an
+        "UNPROCESSABLE_ENTITY".
+    """
     acceptable_characters = string.ascii_letters + string.digits + '+/='
     for character in encoded_data:
         if character not in acceptable_characters:
