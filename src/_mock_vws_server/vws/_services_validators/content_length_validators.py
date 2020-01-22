44505ecdf8c3139bcbf631ea61d11e7e207934dc
Adam Dangoor <adamdangoor@gmail.com>
2020-01-22 11:39:17 +0000

    A bunch of passing tests

---
+++ src/_mock_vws_server/vws/_services_validators/content_length_validators.py
@@ -0,0 +1,119 @@
+"""
+Content-Length header validators to use in the mock.
+"""
+
+import uuid
+from typing import Any, Callable, Dict, Tuple
+
+import wrapt
+from requests import codes
+from requests_mock.request import _RequestObjectProxy
+from requests_mock.response import _Context
+
+from .._constants import ResultCodes
+from .._mock_common import json_dump
+from flask import request
+
+
+@wrapt.decorator
+def validate_content_length_header_is_int(
+    wrapped: Callable[..., str],
+    instance: Any,  # pylint: disable=unused-argument
+    args: Tuple[_RequestObjectProxy, _Context],
+    kwargs: Dict,
+) -> str:
+    """
+    Validate the ``Content-Length`` header is an integer.
+
+    Args:
+        wrapped: An endpoint function for `requests_mock`.
+        instance: The class that the endpoint function is in.
+        args: The arguments given to the endpoint function.
+        kwargs: The keyword arguments given to the endpoint function.
+
+    Returns:
+        The result of calling the endpoint.
+        A ``BAD_REQUEST`` response if the content length header is not an
+        integer.
+    """
+
+    body_length = len(request.data if request.data else '')
+    given_content_length = request.headers.get('Content-Length', body_length)
+
+    try:
+        int(given_content_length)
+    except ValueError:
+        # TODO construct response
+        # context.headers = {'Connection': 'Close'}
+        return '', codes.BAD_REQUEST
+
+    return wrapped(*args, **kwargs)
+
+
+@wrapt.decorator
+def validate_content_length_header_not_too_large(
+    wrapped: Callable[..., str],
+    instance: Any,  # pylint: disable=unused-argument
+    args: Tuple[_RequestObjectProxy, _Context],
+    kwargs: Dict,
+) -> str:
+    """
+    Validate the ``Content-Length`` header is not too large.
+
+    Args:
+        wrapped: An endpoint function for `requests_mock`.
+        instance: The class that the endpoint function is in.
+        args: The arguments given to the endpoint function.
+        kwargs: The keyword arguments given to the endpoint function.
+
+    Returns:
+        The result of calling the endpoint.
+        A ``GATEWAY_TIMEOUT`` response if the given content length header says
+        that the content length is greater than the body length.
+    """
+
+    body_length = len(request.data if request.data else '')
+    given_content_length = request.headers.get('Content-Length', body_length)
+    given_content_length_value = int(given_content_length)
+    if given_content_length_value > body_length:
+        # TODO construct a response object
+        # context.headers = {'Connection': 'keep-alive'}
+        return '', codes.GATEWAY_TIMEOUT
+
+    return wrapped(*args, **kwargs)
+
+
+@wrapt.decorator
+def validate_content_length_header_not_too_small(
+    wrapped: Callable[..., str],
+    instance: Any,  # pylint: disable=unused-argument
+    args: Tuple[_RequestObjectProxy, _Context],
+    kwargs: Dict,
+) -> str:
+    """
+    Validate the ``Content-Length`` header is not too small.
+
+    Args:
+        wrapped: An endpoint function for `requests_mock`.
+        instance: The class that the endpoint function is in.
+        args: The arguments given to the endpoint function.
+        kwargs: The keyword arguments given to the endpoint function.
+
+    Returns:
+        The result of calling the endpoint.
+        An ``UNAUTHORIZED`` response if the given content length header says
+        that the content length is smaller than the body length.
+    """
+
+    body_length = len(request.data if request.data else '')
+    given_content_length = request.headers.get('Content-Length', body_length)
+    given_content_length_value = int(given_content_length)
+
+    if given_content_length_value < body_length:
+        body = {
+            'transaction_id': uuid.uuid4().hex,
+            'result_code': ResultCodes.AUTHENTICATION_FAILURE.value,
+        }
+        return json_dump(body), codes.UNAUTHORIZED
+
+    return wrapped(*args, **kwargs)
---
+++ src/_mock_vws_server/vws/_services_validators/date_validators.py
@@ -0,0 +1,128 @@
+"""
+Validators of the date header to use in the mock services API.
+"""
+
+import datetime
+import uuid
+from typing import Any, Callable, Dict, Tuple
+
+import pytz
+import wrapt
+from flask import request
+from requests import codes
+from requests_mock.request import _RequestObjectProxy
+from requests_mock.response import _Context
+
+from mock_vws._constants import ResultCodes
+from mock_vws._mock_common import json_dump
+
+
+@wrapt.decorator
+def validate_date_header_given(
+    wrapped: Callable[..., str],
+    instance: Any,  # pylint: disable=unused-argument
+    args: Tuple[_RequestObjectProxy, _Context],
+    kwargs: Dict,
+) -> str:
+    """
+    Validate the date header is given to a VWS endpoint.
+
+    Args:
+        wrapped: An endpoint function for `requests_mock`.
+        instance: The class that the endpoint function is in.
+        args: The arguments given to the endpoint function.
+        kwargs: The keyword arguments given to the endpoint function.
+
+    Returns:
+        The result of calling the endpoint.
+        A `BAD_REQUEST` response if the date is not given.
+    """
+
+    if 'Date' in request.headers:
+        return wrapped(*args, **kwargs)
+
+    body = {
+        'transaction_id': uuid.uuid4().hex,
+        'result_code': ResultCodes.FAIL.value,
+    }
+    return json_dump(body), codes.BAD_REQUEST
+
+
+@wrapt.decorator
+def validate_date_format(
+    wrapped: Callable[..., str],
+    instance: Any,  # pylint: disable=unused-argument
+    args: Tuple[_RequestObjectProxy, _Context],
+    kwargs: Dict,
+) -> str:
+    """
+    Validate the format of the date header given to a VWS endpoint.
+
+    Args:
+        wrapped: An endpoint function for `requests_mock`.
+        instance: The class that the endpoint function is in.
+        args: The arguments given to the endpoint function.
+        kwargs: The keyword arguments given to the endpoint function.
+
+    Returns:
+        The result of calling the endpoint.
+        A `BAD_REQUEST` response if the date is in the wrong format.
+        A `FORBIDDEN` response if the date is out of range.
+    """
+
+    date_header = request.headers['Date']
+    date_format = '%a, %d %b %Y %H:%M:%S GMT'
+    try:
+        datetime.datetime.strptime(date_header, date_format)
+    except ValueError:
+        body = {
+            'transaction_id': uuid.uuid4().hex,
+            'result_code': ResultCodes.FAIL.value,
+        }
+        return json_dump(body), codes.BAD_REQUEST
+
+    return wrapped(*args, **kwargs)
+
+
+@wrapt.decorator
+def validate_date_in_range(
+    wrapped: Callable[..., str],
+    instance: Any,  # pylint: disable=unused-argument
+    args: Tuple[_RequestObjectProxy, _Context],
+    kwargs: Dict,
+) -> str:
+    """
+    Validate the date header given to a VWS endpoint is in range.
+
+    Args:
+        wrapped: An endpoint function for `requests_mock`.
+        instance: The class that the endpoint function is in.
+        args: The arguments given to the endpoint function.
+        kwargs: The keyword arguments given to the endpoint function.
+
+    Returns:
+        The result of calling the endpoint.
+        A `FORBIDDEN` response if the date is out of range.
+    """
+
+    date_from_header = datetime.datetime.strptime(
+        request.headers['Date'],
+        '%a, %d %b %Y %H:%M:%S GMT',
+    )
+
+    gmt = pytz.timezone('GMT')
+    now = datetime.datetime.now(tz=gmt)
+    date_from_header = date_from_header.replace(tzinfo=gmt)
+    time_difference = now - date_from_header
+
+    maximum_time_difference = datetime.timedelta(minutes=5)
+
+    if abs(time_difference) >= maximum_time_difference:
+
+        body = {
+            'transaction_id': uuid.uuid4().hex,
+            'result_code': ResultCodes.REQUEST_TIME_TOO_SKEWED.value,
+        }
+        return json_dump(body), codes.FORBIDDEN
+
+    return wrapped(*args, **kwargs)
