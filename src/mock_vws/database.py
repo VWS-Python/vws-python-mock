8c37fb42c74bc78cbeef91d8ca54a9c0923e5b18
Adam Dangoor <adamdangoor@gmail.com>
2020-02-21 14:46:23 +0000

    Progress towards using dataclass for VuforiaDatabase

--- src/mock_vws/database.py
+++ src/mock_vws/database.py
@@ -3,73 +3,30 @@ Utilities for managing mock Vuforia databases.
 """
 
 import uuid
-from typing import List, Optional
+from dataclasses import dataclass, field
+from typing import List
 
 from .states import States
 from .target import Target
 
 
+def _random_hex() -> str:
+    """
+    Return a random hex value.
+    """
+    return uuid.uuid4().hex
+
+
+@dataclass
 class VuforiaDatabase:
     """
     Credentials for VWS APIs.
     """
 
-    database_name: str
-    server_access_key: str
-    server_secret_key: str
-    client_access_key: str
-    client_secret_key: str
-    targets: List[Target]
-    state: States
-
-    def __init__(
-        self,
-        server_access_key: Optional[str] = None,
-        server_secret_key: Optional[str] = None,
-        client_access_key: Optional[str] = None,
-        client_secret_key: Optional[str] = None,
-        database_name: Optional[str] = None,
-        state: States = States.WORKING,
-    ) -> None:
-        """
-        Args:
-            database_name: The name of a VWS target manager database name.
-            server_access_key: A VWS server access key.
-            server_secret_key: A VWS server secret key.
-            client_access_key: A VWS client access key.
-            client_secret_key: A VWS client secret key.
-            state: The state of the database.
-
-        Attributes:
-            database_name (str): The name of a VWS target manager database.
-            server_access_key (bytes): A VWS server access key.
-            server_secret_key (bytes): A VWS server secret key.
-            client_access_key (bytes): A VWS client access key.
-            client_secret_key (bytes): A VWS client secret key.
-            targets (typing.List[Target]): The
-                :class:`~mock_vws.target.Target` s in the database.
-            state (States): The state of the database.
-        """
-
-        if database_name is None:
-            database_name = uuid.uuid4().hex
-
-        if server_access_key is None:
-            server_access_key = uuid.uuid4().hex
-
-        if server_secret_key is None:
-            server_secret_key = uuid.uuid4().hex
-
-        if client_access_key is None:
-            client_access_key = uuid.uuid4().hex
-
-        if client_secret_key is None:
-            client_secret_key = uuid.uuid4().hex
-
-        self.server_access_key = server_access_key
-        self.server_secret_key = server_secret_key
-        self.client_access_key = client_access_key
-        self.client_secret_key = client_secret_key
-        self.database_name = database_name
-        self.targets: List[Target] = []
-        self.state = state
+    database_name: str = field(default_factory=_random_hex)
+    server_access_key: str = field(default_factory=_random_hex)
+    server_secret_key: str = field(default_factory=_random_hex)
+    client_access_key: str = field(default_factory=_random_hex)
+    client_secret_key: str = field(default_factory=_random_hex)
+    targets: List[Target] = field(default_factory=list)
+    state: States = States.WORKING
