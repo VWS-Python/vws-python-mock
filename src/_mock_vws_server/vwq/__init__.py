7e838d53a464b9cfab6f214cd300b1dcba0da6b2
Adam Dangoor <adamdangoor@gmail.com>
2020-02-26 17:54:09 +0000

    Progress towards query

--- src/_mock_vws_server/vwq/__init__.py
+++ src/_mock_vws_server/vwq/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from requests import codes
+from typing import Tuple
 
 CLOUDRECO_FLASK_APP = Flask(__name__)
+
+@CLOUDRECO_FLASK_APP.route('/v1/query', methods=['POST'])
+def query() -> Tuple[str, int]:
+    return '', codes.OK
