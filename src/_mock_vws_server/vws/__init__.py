7a459f5a9f1874bc0a6e808e36b007629b16d408
Adam Dangoor <adamdangoor@gmail.com>
2020-03-25 21:30:05 +0000

    Progress towards using new style

--- src/_mock_vws_server/vws/__init__.py
+++ src/_mock_vws_server/vws/__init__.py
@@ -19,43 +19,7 @@ from mock_vws.target import Target
 
 from ._constants import STORAGE_BASE_URL
 from ._databases import get_all_databases
-from ._services_validators import (
-    validate_active_flag,
-    validate_metadata_encoding,
-    validate_metadata_size,
-    validate_metadata_type,
-    validate_name_characters_in_range,
-    validate_name_length,
-    validate_name_type,
-    validate_not_invalid_json,
-    validate_project_state,
-    validate_width,
-)
-from ._services_validators.auth_validators import (
-    validate_auth_header_exists,
-    validate_auth_header_has_signature,
-)
-from ._services_validators.content_length_validators import (
-    validate_content_length_header_is_int,
-    validate_content_length_header_not_too_large,
-    validate_content_length_header_not_too_small,
-)
-from ._services_validators.content_type_validators import (
-    validate_content_type_header_given,
-)
-from ._services_validators.date_validators import (
-    validate_date_format,
-    validate_date_header_given,
-    validate_date_in_range,
-)
-from ._services_validators.image_validators import (
-    validate_image_color_space,
-    validate_image_data_type,
-    validate_image_encoding,
-    validate_image_format,
-    validate_image_is_image,
-    validate_image_size,
-)
+from mock_vws._services_validators import run_services_validators
 
 VWS_FLASK_APP = Flask(__name__)
 JSON_SCHEMA = JsonSchema(VWS_FLASK_APP)
@@ -82,36 +46,17 @@ ADD_TARGET_SCHEMA = {
 
 
 @VWS_FLASK_APP.before_request
-@validate_content_length_header_is_int
-@validate_content_length_header_not_too_large
-@validate_content_length_header_not_too_small
-@validate_auth_header_exists
-@validate_auth_header_has_signature
-# @validate_access_key_exists
-@validate_not_invalid_json
-@validate_date_header_given
-@validate_date_format
-@validate_date_in_range
-@validate_content_type_header_given
-@validate_width
-# TODO is validating the name type needed given JSON schema?
-@validate_name_type
-@validate_name_length
-@validate_name_characters_in_range
-@validate_image_data_type
-@validate_image_encoding
-@validate_image_is_image
-@validate_image_format
-@validate_image_color_space
-@validate_image_size
-@validate_active_flag
-@validate_metadata_type
-@validate_metadata_encoding
-@validate_metadata_size
-# @validate_authorization
-@validate_project_state
 def validate_request() -> None:
-    pass
+    databases = get_all_databases()
+    run_services_validators(
+        request_text=request.data.decode(),
+        request_headers=dict(request.headers),
+        # TODO not sure about this one
+        request_body=request.data,
+        request_method=request.method,
+        request_path=request.path,
+        databases=databases,
+    )
     # decorators = [
     #     # parse_target_id,
     #     # update_request_count,
