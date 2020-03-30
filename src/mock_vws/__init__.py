5855c7cfd7c8328c3a29581c9a331acd430bbad3
Adam Dangoor <adamdangoor@gmail.com>
2020-03-30 22:26:26 +0100

    Move things around to allow flask server to easily fit in

--- src/mock_vws/__init__.py
+++ src/mock_vws/__init__.py
@@ -2,7 +2,7 @@
 Tools for using a fake implementation of Vuforia.
 """
 
-from ._decorators import MockVWS
+from mock_vws._requests_mock_server.decorators import MockVWS
 
 __all__ = [
     'MockVWS',
