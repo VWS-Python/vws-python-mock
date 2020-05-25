870fdfd7c5c9b9673f0d493f892c70777fcdf7d3
Adam Dangoor <adamdangoor@gmail.com>
2020-05-25 16:00:08 +0100

    Print environment so we can see if we can differentiate matrix number

--- ci/set_secrets_file.py
+++ ci/set_secrets_file.py
@@ -5,6 +5,7 @@ Move the right secrets file into place for Travis CI.
 import os
 import shutil
 from pathlib import Path
+import sys
 
 
 def move_secrets_file() -> None:
@@ -12,11 +13,14 @@ def move_secrets_file() -> None:
     Move the right secrets file to the current directory.
     """
     # TODO change this
-    travis_job_number = os.environ['TRAVIS_JOB_NUMBER']
+    print(os.environ)
+    sys.exit(0)
+    travis_job_number = 1  # os.environ['TRAVIS_JOB_NUMBER']
     travis_builder_number = travis_job_number.split('.')[-1]
     secrets_dir = Path('ci_secrets')
     secrets_path = secrets_dir / f'vuforia_secrets_{travis_builder_number}.env'
     shutil.copy(secrets_path, './vuforia_secrets.env')
+    # TODO this is temporary
 
 
 if __name__ == '__main__':
