cc06af2f4b7b6208d8aa8e4af4478f4a7946fec2
Adam Dangoor <adamdangoor@gmail.com>
2020-05-26 16:52:39 +0100

    Move lint from Travis CI to GitHub

--- ci/set_secrets_file.py
+++ ci/set_secrets_file.py
@@ -13,8 +13,9 @@ def move_secrets_file() -> None:
     """
     travis_job_number = os.environ['TRAVIS_JOB_NUMBER']
     travis_builder_number = travis_job_number.split('.')[-1]
+    secret_file = f'vuforia_secrets_{travis_builder_number + 1}.env'
     secrets_dir = Path('ci_secrets')
-    secrets_path = secrets_dir / f'vuforia_secrets_{travis_builder_number}.env'
+    secrets_path = secrets_dir / secret_file
     shutil.copy(secrets_path, './vuforia_secrets.env')
 
 
