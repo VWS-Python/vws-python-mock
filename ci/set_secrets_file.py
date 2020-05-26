4e9dbe0e04bf025ec5d9ff0c4bcd71e3dd5272dd
Adam Dangoor <adamdangoor@gmail.com>
2020-05-26 16:56:59 +0100

    Fix type issue

--- ci/set_secrets_file.py
+++ ci/set_secrets_file.py
@@ -12,7 +12,7 @@ def move_secrets_file() -> None:
     Move the right secrets file to the current directory.
     """
     travis_job_number = os.environ['TRAVIS_JOB_NUMBER']
-    travis_builder_number = travis_job_number.split('.')[-1]
+    travis_builder_number = int(travis_job_number.split('.')[-1])
     secret_file = f'vuforia_secrets_{travis_builder_number + 1}.env'
     secrets_dir = Path('ci_secrets')
     secrets_path = secrets_dir / secret_file
