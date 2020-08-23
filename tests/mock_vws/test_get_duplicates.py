03a56acd4b61c1c88b86a0b163545f45f70837b9
Adam Dangoor <adamdangoor@gmail.com>
2020-08-23 03:48:52 +0100

    Take use of add_target_to_vws out of some tests in test_get_duplicates

--- tests/mock_vws/test_get_duplicates.py
+++ tests/mock_vws/test_get_duplicates.py
@@ -32,51 +32,36 @@ class TestDuplicates:
 
         In the mock, "similar" means that the images are exactly the same.
         """
-        image_data = high_quality_image.read()
-        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+        image_data = high_quality_image
+        different_image_data = image_file_success_state_low_rating
 
-        different = image_file_success_state_low_rating.read()
-        different_data_encoded = base64.b64encode(different).decode('ascii')
-
-        original_data = {
-            'name': str(uuid.uuid4()),
-            'width': 1,
-            'image': image_data_encoded,
-        }
-
-        similar_data = {
-            'name': str(uuid.uuid4()),
-            'width': 1,
-            'image': image_data_encoded,
-        }
-
-        different_data = {
-            'name': str(uuid.uuid4()),
-            'width': 1,
-            'image': different_data_encoded,
-        }
-
-        original_add_resp = add_target_to_vws(
-            vuforia_database=vuforia_database,
-            data=original_data,
-        )
-
-        similar_add_resp = add_target_to_vws(
-            vuforia_database=vuforia_database,
-            data=similar_data,
-        )
-
-        different_add_resp = add_target_to_vws(
-            vuforia_database=vuforia_database,
-            data=different_data,
-        )
-
-        original_target_id = original_add_resp.json()['target_id']
-        similar_target_id = similar_add_resp.json()['target_id']
-        different_target_id = different_add_resp.json()['target_id']
         vws_client = VWS(
             server_access_key=vuforia_database.server_access_key,
             server_secret_key=vuforia_database.server_secret_key,
+        )
+
+        original_target_id = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=image_data,
+            active_flag=True,
+            application_metadata=None,
+        )
+
+        similar_target_id = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=image_data,
+            active_flag=True,
+            application_metadata=None,
+        )
+
+        different_target_id = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=different_image_data,
+            active_flag=True,
+            application_metadata=None,
         )
 
         vws_client.wait_for_target_processed(target_id=original_target_id)
@@ -97,37 +82,25 @@ class TestDuplicates:
         """
         Targets are not duplicates if the status is not 'success'.
         """
-        image_data = image_file_failed_state.read()
-        image_data_encoded = base64.b64encode(image_data).decode('ascii')
-
-        original_data = {
-            'name': str(uuid.uuid4()),
-            'width': 1,
-            'image': image_data_encoded,
-        }
-
-        similar_data = {
-            'name': str(uuid.uuid4()),
-            'width': 1,
-            'image': image_data_encoded,
-        }
-
-        original_add_resp = add_target_to_vws(
-            vuforia_database=vuforia_database,
-            data=original_data,
-        )
-
-        similar_add_resp = add_target_to_vws(
-            vuforia_database=vuforia_database,
-            data=similar_data,
-        )
-
-        original_target_id = original_add_resp.json()['target_id']
-        similar_target_id = similar_add_resp.json()['target_id']
-
         vws_client = VWS(
             server_access_key=vuforia_database.server_access_key,
             server_secret_key=vuforia_database.server_secret_key,
+        )
+
+        original_target_id = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=image_file_failed_state,
+            active_flag=True,
+            application_metadata=None,
+        )
+
+        similar_target_id = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=image_file_failed_state,
+            active_flag=True,
+            application_metadata=None,
         )
 
         vws_client.wait_for_target_processed(target_id=original_target_id)
@@ -277,40 +250,27 @@ class TestProcessing:
         If a target is in the processing state, it can have duplicates.
         Targets can have duplicates in the processing state.
         """
-        image_data = high_quality_image.read()
-        image_data_encoded = base64.b64encode(image_data).decode('ascii')
-
-        data_1 = {
-            'name': str(uuid.uuid4()),
-            'width': 1,
-            'image': image_data_encoded,
-        }
-
-        data_2 = {
-            'name': str(uuid.uuid4()),
-            'width': 1,
-            'image': image_data_encoded,
-        }
-
-        resp_1 = add_target_to_vws(
-            vuforia_database=vuforia_database,
-            data=data_1,
-        )
-
-        processed_target_id = resp_1.json()['target_id']
-
         vws_client = VWS(
             server_access_key=vuforia_database.server_access_key,
             server_secret_key=vuforia_database.server_secret_key,
         )
+
+        processed_target_id = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=high_quality_image,
+            active_flag=True,
+            application_metadata=None,
+        )
         vws_client.wait_for_target_processed(target_id=processed_target_id)
 
-        resp_2 = add_target_to_vws(
-            vuforia_database=vuforia_database,
-            data=data_2,
+        processing_target_id = vws_client.add_target(
+            name=uuid.uuid4().hex,
+            width=1,
+            image=high_quality_image,
+            active_flag=True,
+            application_metadata=None,
         )
-
-        processing_target_id = resp_2.json()['target_id']
 
         duplicates = vws_client.get_duplicate_targets(
             target_id=processed_target_id,
@@ -319,7 +279,7 @@ class TestProcessing:
         assert duplicates == []
 
         duplicates = vws_client.get_duplicate_targets(
-            target_id=processed_target_id,
+            target_id=processing_target_id,
         )
 
         target_details = vws_client.get_target_record(
@@ -327,7 +287,7 @@ class TestProcessing:
         )
 
         assert target_details.status.value == TargetStatuses.PROCESSING.value
-        assert duplicates == []
+        assert duplicates == [processed_target_id]
 
 
 @pytest.mark.usefixtures('verify_mock_vuforia')
