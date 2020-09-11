b96708ed06263527deff291eadfca98a2b2f7986
Adam Dangoor <adamdangoor@gmail.com>
2020-09-11 08:43:46 +0100

    Move a bunch of logic about target databases into the VuforiaDatabase class, away from the requests-mock specific code

--- src/mock_vws/database.py
+++ src/mock_vws/database.py
@@ -6,8 +6,9 @@ import uuid
 from dataclasses import dataclass, field
 from typing import Set
 
-from .states import States
-from .target import Target
+from mock_vws._constants import TargetStatuses
+from mock_vws.states import States
+from mock_vws.target import Target
 
 
 def _random_hex() -> str:
@@ -32,3 +33,63 @@ class VuforiaDatabase:
     client_secret_key: str = field(default_factory=_random_hex, repr=False)
     targets: Set[Target] = field(default_factory=set, hash=False)
     state: States = States.WORKING
+
+    request_quota = 100000
+    reco_threshold = 1000
+    current_month_recos = 0
+    previous_month_recos = 0
+    total_recos = 0
+    target_quota = 1000
+
+    @property
+    def not_deleted_targets(self) -> Set[Target]:
+        """
+        All targets which have not been deleted.
+        """
+        return set(target for target in self.targets if not target.delete_date)
+
+    @property
+    def active_targets(self) -> Set[Target]:
+        """
+        All active targets.
+        """
+        return set(
+            target
+            for target in self.not_deleted_targets
+            if target.status == TargetStatuses.SUCCESS.value
+            and target.active_flag
+        )
+
+    @property
+    def inactive_targets(self) -> Set[Target]:
+        """
+        All inactive targets.
+        """
+        return set(
+            target
+            for target in self.not_deleted_targets
+            if target.status == TargetStatuses.SUCCESS.value
+            and not target.active_flag
+        )
+
+    @property
+    def failed_targets(self) -> Set[Target]:
+        """
+        All failed targets.
+        """
+        return set(
+            target
+            for target in self.not_deleted_targets
+            if target.status == TargetStatuses.FAILED.value
+        )
+
+    @property
+    def processing_targets(self) -> Set[Target]:
+        """
+        All processing targets.
+        """
+        return set(
+            target
+            for target in self.not_deleted_targets
+            if target.status == TargetStatuses.PROCESSING.value
+        )
