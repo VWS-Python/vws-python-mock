bfbc6d13bd2e17e148364f97663ed996daf4e1bd
Adam Dangoor <adamdangoor@gmail.com>
2020-09-19 09:36:06 +0100

    Progress towards moving logic of dict dumping to Target and VuforiaDatabase classes

--- src/mock_vws/_flask_server/vws/_databases.py
+++ src/mock_vws/_flask_server/vws/_databases.py
@@ -1,81 +1,15 @@
-import base64
-import datetime
-import io
 from typing import Set
 
 import requests
-from backports.zoneinfo import ZoneInfo
 
 from mock_vws.database import VuforiaDatabase
-from mock_vws.states import States
-from mock_vws.target import Target
 
 from ._constants import STORAGE_BASE_URL
 
 
 def get_all_databases() -> Set[VuforiaDatabase]:
     response = requests.get(url=STORAGE_BASE_URL + '/databases')
-    response_json = response.json()
-    databases = set()
-    for database_dict in response_json:
-        database_name = database_dict['database_name']
-        server_access_key = database_dict['server_access_key']
-        server_secret_key = database_dict['server_secret_key']
-        client_access_key = database_dict['client_access_key']
-        client_secret_key = database_dict['client_secret_key']
-        state = States(database_dict['state_value'])
-
-        new_database = VuforiaDatabase(
-            database_name=database_name,
-            server_access_key=server_access_key,
-            server_secret_key=server_secret_key,
-            client_access_key=client_access_key,
-            client_secret_key=client_secret_key,
-            state=state,
-        )
-
-        for target_dict in database_dict['targets']:
-            # TODO target.from_dict()
-            name = target_dict['name']
-            active_flag = target_dict['active_flag']
-            width = target_dict['width']
-            image_base64 = target_dict['image_base64']
-            image_bytes = base64.b64decode(image_base64)
-            image = io.BytesIO(image_bytes)
-            processing_time_seconds = target_dict['processing_time_seconds']
-            application_metadata = target_dict['application_metadata']
-
-            target = Target(
-                name=name,
-                active_flag=active_flag,
-                width=width,
-                image=image,
-                processing_time_seconds=processing_time_seconds,
-                application_metadata=application_metadata,
-            )
-            target.target_id = target_dict['target_id']
-            gmt = ZoneInfo('GMT')
-            target.last_modified_date = datetime.datetime.fromisoformat(
-                target_dict['last_modified_date'],
-            )
-            target.last_modified_date = target.last_modified_date.replace(
-                tzinfo=gmt,
-            )
-            target.upload_date = datetime.datetime.fromisoformat(
-                target_dict['upload_date'],
-            )
-            target.processed_tracking_rating = target_dict[
-                'processed_tracking_rating'
-            ]
-            target.upload_date = target.upload_date.replace(tzinfo=gmt)
-            delete_date_optional = target_dict['delete_date_optional']
-            if delete_date_optional:
-                target.delete_date = datetime.datetime.fromisoformat(
-                    delete_date_optional,
-                )
-                target.delete_date = target.delete_date.replace(tzinfo=gmt)
-            new_database.targets.add(target)
-
-        databases.add(new_database)
-
-    return databases
+    return set(
+        VuforiaDatabase.from_dict(database_dict=database_dict)
+        for database_dict in response.json()
+    )
