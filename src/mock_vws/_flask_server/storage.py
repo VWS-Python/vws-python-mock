aa9bdeb1727c9dd6a78581ede6fd12621e744298
Adam Dangoor <adamdangoor@gmail.com>
2020-09-20 21:54:57 +0100

    Handle immutable Target class

--- src/mock_vws/_flask_server/storage.py
+++ src/mock_vws/_flask_server/storage.py
@@ -86,8 +86,8 @@ def create_target(database_name: str) -> Tuple[str, int]:
         active_flag=request.json['active_flag'],
         processing_time_seconds=request.json['processing_time_seconds'],
         application_metadata=request.json['application_metadata'],
+        target_id=request.json['target_id'],
     )
-    target.target_id = request.json['target_id']
     database.targets.add(target)
 
     return jsonify(target.to_dict()), HTTPStatus.CREATED
@@ -109,8 +109,27 @@ def delete_target(database_name: str, target_id: str) -> Tuple[str, int]:
     [target] = [
         target for target in database.targets if target.target_id == target_id
     ]
-    target.delete()
-    return jsonify(target.to_dict()), HTTPStatus.OK
+    now = datetime.datetime.now(tz=target.upload_date.tzinfo)
+    new_target = Target(
+        active_flag=target.active_flag,
+        application_metadata=target.application_metadata,
+        image=target.image,
+        name=target.name,
+        processing_time_seconds=target.processing_time_seconds,
+        width=target.width,
+        current_month_recos=target.current_month_recos,
+        delete_date=now,
+        last_modified_date=target.last_modified_date,
+        previous_month_recos=target.previous_month_recos,
+        processed_tracking_rating=target.processed_tracking_rating,
+        reco_rating=target.reco_rating,
+        target_id=target.target_id,
+        total_recos=target.total_recos,
+        upload_date=target.upload_date,
+    )
+    database.targets.remove(target)
+    database.targets.add(new_target)
+    return jsonify(new_target.to_dict()), HTTPStatus.OK
 
 
 @STORAGE_FLASK_APP.route(
@@ -130,34 +149,59 @@ def update_target(database_name: str, target_id: str) -> Tuple[str, int]:
         target for target in database.targets if target.target_id == target_id
     ]
 
-    if 'name' in request.json:
-        target.name = request.json['name']
+    width = target.width
+    if 'width' in request.json():
+        width = request.json()['width']
 
-    if 'active_flag' in request.json:
-        target.active_flag = bool(request.json['active_flag'])
+    active_flag = target.active_flag
+    if 'active_flag' in request.json():
+        active_flag = request.json()['active_flag']
 
-    if 'width' in request.json:
-        target.width = float(request.json['width'])
+    application_metadata = target.application_metadata
+    if 'application_metadata' in request.json():
+        application_metadata = request.json()['application_metadata']
 
-    if 'application_metadata' in request.json:
-        target.application_metadata = request.json['application_metadata']
+    name = target.name
+    if 'name' in request.json():
+        name = request.json()['name']
 
-    if 'image' in request.json:
-        decoded = base64.b64decode(request.json['image'])
+    image_file = target.image
+    if 'image' in request.json():
+        image = request.json()['image']
+        decoded = base64.b64decode(image)
         image_file = io.BytesIO(decoded)
-        target.image = image_file
 
     # In the real implementation, the tracking rating can stay the same.
     # However, for demonstration purposes, the tracking rating changes but
     # when the target is updated.
     available_values = list(set(range(6)) - set([target.tracking_rating]))
-    target.processed_tracking_rating = random.choice(available_values)
+    processed_tracking_rating = random.choice(available_values)
 
     gmt = ZoneInfo('GMT')
-    now = datetime.datetime.now(tz=gmt)
-    target.last_modified_date = now
+    last_modified_date = datetime.datetime.now(tz=gmt)
 
-    return jsonify(target.to_dict()), HTTPStatus.OK
+    new_target = Target(
+        active_flag=active_flag,
+        application_metadata=application_metadata,
+        image=image_file,
+        name=name,
+        processing_time_seconds=target.processing_time_seconds,
+        width=width,
+        current_month_recos=target.current_month_recos,
+        delete_date=target.delete_date,
+        last_modified_date=last_modified_date,
+        previous_month_recos=target.previous_month_recos,
+        processed_tracking_rating=processed_tracking_rating,
+        reco_rating=target.reco_rating,
+        target_id=target.target_id,
+        total_recos=target.total_recos,
+        upload_date=target.upload_date,
+    )
+
+    database.targets.remove(target)
+    database.targets.add(new_target)
+
+    return jsonify(new_target.to_dict()), HTTPStatus.OK
 
 if __name__ == '__main__':  # pragma: no cover
     STORAGE_FLASK_APP.run(debug=True, host='0.0.0.0')
