bf417ca7acd35542af1e7f7cada04d42bfd80f0d
Adam Dangoor <adamdangoor@gmail.com>
2020-09-21 20:18:48 +0100

    Expanding test

--- tests/mock_vws/test_docker.py
+++ tests/mock_vws/test_docker.py
@@ -5,4 +5,12 @@ Tests for running the mock server in Docker.
 import docker
 
 def test_build_and_run():
+    client = docker.from_env()
+    # Build containers
+    dockerfile_path = ...
+    image, build_logs = client.images.build(path=dockerfile_path)
+    container = client.containers.run(image=image, detach=True)
+    # Run containers
+
+    # Add target using vws_python
     pass
