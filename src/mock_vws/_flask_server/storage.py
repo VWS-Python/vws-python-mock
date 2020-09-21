226a31d4262fade5b473334d10165c8f5a731edd
Adam Dangoor <adamdangoor@gmail.com>
2020-09-21 15:48:59 +0100

    Add a Flask-based backend with no user interface yet which passes the mock tests

---
+++ src/mock_vws/_flask_server/storage.py
@@ -0,0 +1,173 @@
+"""
+Storage layer for the mock Vuforia Flask application.
+"""
+
+import base64
+import dataclasses
+import datetime
+import io
+import random
+from http import HTTPStatus
+from typing import List, Tuple
+
+from backports.zoneinfo import ZoneInfo
+from flask import Flask, jsonify, request
+
+from mock_vws.database import VuforiaDatabase
+from mock_vws.states import States
+from mock_vws.target import Target
+
+STORAGE_FLASK_APP = Flask(__name__)
+
+VUFORIA_DATABASES: List[VuforiaDatabase] = []
+
+
+@STORAGE_FLASK_APP.route('/reset', methods=['POST'])
+def reset() -> Tuple[str, int]:
+    """
+    Reset the back-end to a state of no databases.
+    """
+    VUFORIA_DATABASES.clear()
+    return '', HTTPStatus.OK
+
+
+@STORAGE_FLASK_APP.route('/databases', methods=['GET'])
+def get_databases() -> Tuple[str, int]:
+    """
+    Return a list of all databases.
+    """
+    databases = [database.to_dict() for database in VUFORIA_DATABASES]
+    return jsonify(databases), HTTPStatus.OK
+
+
+@STORAGE_FLASK_APP.route('/databases', methods=['POST'])
+def create_database() -> Tuple[str, int]:
+    """
+    Create a new database.
+    """
+    server_access_key = request.json['server_access_key']
+    server_secret_key = request.json['server_secret_key']
+    client_access_key = request.json['client_access_key']
+    client_secret_key = request.json['client_secret_key']
+    database_name = request.json['database_name']
+    state = States[request.json['state_name']]
+
+    database = VuforiaDatabase(
+        server_access_key=server_access_key,
+        server_secret_key=server_secret_key,
+        client_access_key=client_access_key,
+        client_secret_key=client_secret_key,
+        database_name=database_name,
+        state=state,
+    )
+    VUFORIA_DATABASES.append(database)
+    return jsonify(database.to_dict()), HTTPStatus.CREATED
+
+
+@STORAGE_FLASK_APP.route(
+    '/databases/<string:database_name>/targets',
+    methods=['POST'],
+)
+def create_target(database_name: str) -> Tuple[str, int]:
+    """
+    Create a new target in a given database.
+    """
+    [database] = [
+        database
+        for database in VUFORIA_DATABASES
+        if database.database_name == database_name
+    ]
+    image_base64 = request.json['image_base64']
+    image_bytes = base64.b64decode(image_base64)
+    image = io.BytesIO(image_bytes)
+    target = Target(
+        name=request.json['name'],
+        width=request.json['width'],
+        image=image,
+        active_flag=request.json['active_flag'],
+        processing_time_seconds=request.json['processing_time_seconds'],
+        application_metadata=request.json['application_metadata'],
+        target_id=request.json['target_id'],
+    )
+    database.targets.add(target)
+
+    return jsonify(target.to_dict()), HTTPStatus.CREATED
+
+
+@STORAGE_FLASK_APP.route(
+    '/databases/<string:database_name>/targets/<string:target_id>',
+    methods=['DELETE'],
+)
+def delete_target(database_name: str, target_id: str) -> Tuple[str, int]:
+    """
+    Delete a target.
+    """
+    [database] = [
+        database
+        for database in VUFORIA_DATABASES
+        if database.database_name == database_name
+    ]
+    target = database.get_target(target_id=target_id)
+    now = datetime.datetime.now(tz=target.upload_date.tzinfo)
+    new_target = dataclasses.replace(target, delete_date=now)
+    database.targets.remove(target)
+    database.targets.add(new_target)
+    return jsonify(new_target.to_dict()), HTTPStatus.OK
+
+
+@STORAGE_FLASK_APP.route(
+    '/databases/<string:database_name>/targets/<string:target_id>',
+    methods=['PUT'],
+)
+def update_target(database_name: str, target_id: str) -> Tuple[str, int]:
+    """
+    Update a target.
+    """
+    [database] = [
+        database
+        for database in VUFORIA_DATABASES
+        if database.database_name == database_name
+    ]
+    target = database.get_target(target_id=target_id)
+
+    width = request.json.get('width', target.width)
+    name = request.json.get('name', target.name)
+    active_flag = request.json.get('active_flag', target.active_flag)
+    application_metadata = request.json.get(
+        'application_metadata',
+        target.application_metadata,
+    )
+
+    image_file = target.image
+    if 'image' in request.json:
+        image = request.json['image']
+        decoded = base64.b64decode(image)
+        image_file = io.BytesIO(decoded)
+
+    # In the real implementation, the tracking rating can stay the same.
+    # However, for demonstration purposes, the tracking rating changes but
+    # when the target is updated.
+    available_values = list(set(range(6)) - set([target.tracking_rating]))
+    processed_tracking_rating = random.choice(available_values)
+
+    gmt = ZoneInfo('GMT')
+    last_modified_date = datetime.datetime.now(tz=gmt)
+
+    new_target = dataclasses.replace(
+        target,
+        name=name,
+        width=width,
+        active_flag=active_flag,
+        application_metadata=application_metadata,
+        image=image_file,
+        processed_tracking_rating=processed_tracking_rating,
+        last_modified_date=last_modified_date,
+    )
+
+    database.targets.remove(target)
+    database.targets.add(new_target)
+
+    return jsonify(new_target.to_dict()), HTTPStatus.OK
+
+if __name__ == '__main__':  # pragma: no cover
+    STORAGE_FLASK_APP.run(debug=True, host='0.0.0.0')
---
+++ src/mock_vws/_flask_server/vws.py
@@ -0,0 +1,555 @@
+"""
+A fake implementation of the Vuforia Web Services API.
+
+See
+https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API
+"""
+
+import base64
+import email.utils
+import io
+import json
+import uuid
+from http import HTTPStatus
+from typing import Dict, Final, List, Optional, Set
+
+import requests
+from flask import Flask, Response, request
+from PIL import Image
+
+from mock_vws._constants import ResultCodes, TargetStatuses
+from mock_vws._database_matchers import get_database_matching_server_keys
+from mock_vws._mock_common import json_dump
+from mock_vws._services_validators import run_services_validators
+from mock_vws._services_validators.exceptions import (
+    Fail,
+    TargetStatusNotSuccess,
+    TargetStatusProcessing,
+    ValidatorException,
+)
+from mock_vws.database import VuforiaDatabase
+from mock_vws.target import Target
+
+VWS_FLASK_APP = Flask(import_name=__name__)
+VWS_FLASK_APP.config['PROPAGATE_EXCEPTIONS'] = True
+
+
+# TODO choose something for this - it should actually work in a docker-compose
+# scenario.
+STORAGE_BASE_URL: Final[str] = 'http://todo.com'
+
+
+def get_all_databases() -> Set[VuforiaDatabase]:
+    """
+    Get all database objects from the storage back-end.
+    """
+    response = requests.get(url=STORAGE_BASE_URL + '/databases')
+    return set(
+        VuforiaDatabase.from_dict(database_dict=database_dict)
+        for database_dict in response.json()
+    )
+
+
+class ResponseNoContentTypeAdded(Response):
+    """
+    A custom response type.
+
+    Without this, a content type is added to all responses.
+    Some of our responses need to not have a "Content-Type" header.
+    """
+
+    def __init__(
+        self,
+        response: Optional[str] = None,
+        status: Optional[int] = None,
+        headers: Optional[Dict[str, str]] = None,
+        mimetype: Optional[str] = None,
+        content_type: Optional[str] = None,
+        direct_passthrough: bool = False,
+    ) -> None:
+        if headers:
+            content_type_from_headers = headers.get('Content-Type')
+        else:
+            content_type_from_headers = None
+
+        super().__init__(
+            response=response,
+            status=status,
+            headers=headers,
+            mimetype=mimetype,
+            content_type=content_type,
+            direct_passthrough=direct_passthrough,
+        )
+
+        if (
+            content_type is None
+            and self.headers
+            and 'Content-Type' in self.headers
+            and not content_type_from_headers
+        ):
+            del self.headers['Content-Type']
+
+
+VWS_FLASK_APP.response_class = ResponseNoContentTypeAdded
+
+
+@VWS_FLASK_APP.before_request
+def validate_request() -> None:
+    """
+    Run validators on the request.
+    """
+    request.environ['wsgi.input_terminated'] = True
+    databases = get_all_databases()
+    run_services_validators(
+        request_headers=dict(request.headers),
+        request_body=request.data,
+        request_method=request.method,
+        request_path=request.path,
+        databases=databases,
+    )
+
+
+@VWS_FLASK_APP.errorhandler(ValidatorException)
+def handle_exceptions(exc: ValidatorException) -> Response:
+    """
+    Return the error response associated with the given exception.
+    """
+    return ResponseNoContentTypeAdded(
+        status=exc.status_code.value,
+        response=exc.response_text,
+        headers=exc.headers,
+    )
+
+
+@VWS_FLASK_APP.route('/targets', methods=['POST'])
+def add_target() -> Response:
+    """
+    Add a target.
+
+    Fake implementation of
+    https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API.html#How-To-Add-a-Target
+    """
+    # We do not use ``request.get_json(force=True)`` because this only works
+    # when the content type is given as ``application/json``.
+    databases = get_all_databases()
+    database = get_database_matching_server_keys(
+        request_headers=dict(request.headers),
+        request_body=request.data,
+        request_method=request.method,
+        request_path=request.path,
+        databases=databases,
+    )
+
+    assert isinstance(database, VuforiaDatabase)
+
+    request_json = json.loads(request.data)
+    name = request_json['name']
+    active_flag = request_json.get('active_flag')
+    if active_flag is None:
+        active_flag = True
+
+    image = request_json['image']
+    decoded = base64.b64decode(image)
+    image_file = io.BytesIO(decoded)
+
+    new_target = Target(
+        name=name,
+        width=request_json['width'],
+        image=image_file,
+        active_flag=active_flag,
+        processing_time_seconds=0.2,
+        # TODO add this back:
+        # processing_time_seconds=self._processing_time_seconds,
+        application_metadata=request_json.get('application_metadata'),
+    )
+
+    requests.post(
+        url=f'{STORAGE_BASE_URL}/databases/{database.database_name}/targets',
+        json=new_target.to_dict(),
+    )
+
+    date = email.utils.formatdate(None, localtime=False, usegmt=True)
+    headers = {
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'Server': 'nginx',
+        'Date': date,
+    }
+    body = {
+        'transaction_id': uuid.uuid4().hex,
+        'result_code': ResultCodes.TARGET_CREATED.value,
+        'target_id': new_target.target_id,
+    }
+
+    return Response(
+        status=HTTPStatus.CREATED,
+        response=json_dump(body),
+        headers=headers,
+    )
+
+
+@VWS_FLASK_APP.route('/targets/<string:target_id>', methods=['GET'])
+def get_target(target_id: str) -> Response:
+    """
+    Get details of a target.
+
+    Fake implementation of
+    https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API.html#How-To-Retrieve-a-Target-Record
+    """
+    databases = get_all_databases()
+    database = get_database_matching_server_keys(
+        request_headers=dict(request.headers),
+        request_body=request.data,
+        request_method=request.method,
+        request_path=request.path,
+        databases=databases,
+    )
+
+    assert isinstance(database, VuforiaDatabase)
+    [target] = [
+        target for target in database.targets if target.target_id == target_id
+    ]
+
+    target_record = {
+        'target_id': target.target_id,
+        'active_flag': target.active_flag,
+        'name': target.name,
+        'width': target.width,
+        'tracking_rating': target.tracking_rating,
+        'reco_rating': target.reco_rating,
+    }
+
+    date = email.utils.formatdate(None, localtime=False, usegmt=True)
+    headers = {
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'Server': 'nginx',
+        'Date': date,
+    }
+    body = {
+        'result_code': ResultCodes.SUCCESS.value,
+        'transaction_id': uuid.uuid4().hex,
+        'target_record': target_record,
+        'status': target.status,
+    }
+    return Response(
+        status=HTTPStatus.OK,
+        response=json_dump(body),
+        headers=headers,
+    )
+
+
+@VWS_FLASK_APP.route('/targets/<string:target_id>', methods=['DELETE'])
+def delete_target(target_id: str) -> Response:
+    """
+    Delete a target.
+
+    Fake implementation of
+    https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API.html#How-To-Delete-a-Target
+    """
+    databases = get_all_databases()
+    database = get_database_matching_server_keys(
+        request_headers=dict(request.headers),
+        request_body=request.data,
+        request_method=request.method,
+        request_path=request.path,
+        databases=databases,
+    )
+
+    assert isinstance(database, VuforiaDatabase)
+    [target] = [
+        target for target in database.targets if target.target_id == target_id
+    ]
+
+    if target.status == TargetStatuses.PROCESSING.value:
+        raise TargetStatusProcessing
+
+    delete_url = (
+        f'{STORAGE_BASE_URL}/databases/{database.database_name}/targets/'
+        f'{target_id}'
+    )
+    requests.delete(url=delete_url)
+
+    body = {
+        'transaction_id': uuid.uuid4().hex,
+        'result_code': ResultCodes.SUCCESS.value,
+    }
+    date = email.utils.formatdate(None, localtime=False, usegmt=True)
+    headers = {
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'Server': 'nginx',
+        'Date': date,
+    }
+    return Response(
+        status=HTTPStatus.OK,
+        response=json_dump(body),
+        headers=headers,
+    )
+
+
+@VWS_FLASK_APP.route('/summary', methods=['GET'])
+def database_summary() -> Response:
+    """
+    Get a database summary report.
+
+    Fake implementation of
+    https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API.html#How-To-Get-a-Database-Summary-Report
+    """
+    databases = get_all_databases()
+    database = get_database_matching_server_keys(
+        request_headers=dict(request.headers),
+        request_body=request.data,
+        request_method=request.method,
+        request_path=request.path,
+        databases=databases,
+    )
+
+    assert isinstance(database, VuforiaDatabase)
+    body = {
+        'result_code': ResultCodes.SUCCESS.value,
+        'transaction_id': uuid.uuid4().hex,
+        'name': database.database_name,
+        'active_images': len(database.active_targets),
+        'inactive_images': len(database.inactive_targets),
+        'failed_images': len(database.failed_targets),
+        'target_quota': database.target_quota,
+        'total_recos': database.total_recos,
+        'current_month_recos': database.current_month_recos,
+        'previous_month_recos': database.previous_month_recos,
+        'processing_images': len(database.processing_targets),
+        'reco_threshold': database.reco_threshold,
+        'request_quota': database.request_quota,
+        # We have ``self.request_count`` but Vuforia always shows 0.
+        # This was not always the case.
+        'request_usage': 0,
+    }
+    date = email.utils.formatdate(None, localtime=False, usegmt=True)
+    headers = {
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'Server': 'nginx',
+        'Date': date,
+    }
+    return Response(
+        status=HTTPStatus.OK,
+        response=json_dump(body),
+        headers=headers,
+    )
+
+
+@VWS_FLASK_APP.route('/summary/<string:target_id>', methods=['GET'])
+def target_summary(target_id: str) -> Response:
+    """
+    Get a summary report for a target.
+
+    Fake implementation of
+    https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API.html#How-To-Retrieve-a-Target-Summary-Report
+    """
+    databases = get_all_databases()
+    database = get_database_matching_server_keys(
+        request_headers=dict(request.headers),
+        request_body=request.data,
+        request_method=request.method,
+        request_path=request.path,
+        databases=databases,
+    )
+
+    assert isinstance(database, VuforiaDatabase)
+    [target] = [
+        target for target in database.targets if target.target_id == target_id
+    ]
+    body = {
+        'status': target.status,
+        'transaction_id': uuid.uuid4().hex,
+        'result_code': ResultCodes.SUCCESS.value,
+        'database_name': database.database_name,
+        'target_name': target.name,
+        'upload_date': target.upload_date.strftime('%Y-%m-%d'),
+        'active_flag': target.active_flag,
+        'tracking_rating': target.tracking_rating,
+        'total_recos': target.total_recos,
+        'current_month_recos': target.current_month_recos,
+        'previous_month_recos': target.previous_month_recos,
+    }
+    date = email.utils.formatdate(None, localtime=False, usegmt=True)
+    headers = {
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'Server': 'nginx',
+        'Date': date,
+    }
+    return Response(
+        status=HTTPStatus.OK,
+        response=json_dump(body),
+        headers=headers,
+    )
+
+
+@VWS_FLASK_APP.route('/duplicates/<string:target_id>', methods=['GET'])
+def get_duplicates(target_id: str) -> Response:
+    """
+    Get targets which may be considered duplicates of a given target.
+
+    Fake implementation of
+    https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API.html#How-To-Check-for-Duplicate-Targets
+    """
+    databases = get_all_databases()
+    database = get_database_matching_server_keys(
+        request_headers=dict(request.headers),
+        request_body=request.data,
+        request_method=request.method,
+        request_path=request.path,
+        databases=databases,
+    )
+
+    assert isinstance(database, VuforiaDatabase)
+    [target] = [
+        target for target in database.targets if target.target_id == target_id
+    ]
+    other_targets = set(database.targets) - set([target])
+
+    similar_targets: List[str] = [
+        other.target_id
+        for other in other_targets
+        if Image.open(other.image) == Image.open(target.image)
+        and TargetStatuses.FAILED.value not in (target.status, other.status)
+        and TargetStatuses.PROCESSING.value != other.status
+        and other.active_flag
+    ]
+
+    body = {
+        'transaction_id': uuid.uuid4().hex,
+        'result_code': ResultCodes.SUCCESS.value,
+        'similar_targets': similar_targets,
+    }
+
+    date = email.utils.formatdate(None, localtime=False, usegmt=True)
+    headers = {
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'Server': 'nginx',
+        'Date': date,
+    }
+    return Response(
+        status=HTTPStatus.OK,
+        response=json_dump(body),
+        headers=headers,
+    )
+
+
+@VWS_FLASK_APP.route('/targets', methods=['GET'])
+def target_list() -> Response:
+    """
+    Get a list of all targets.
+
+    Fake implementation of
+    https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API.html#How-To-Get-a-Target-List-for-a-Cloud-Database
+    """
+    databases = get_all_databases()
+    database = get_database_matching_server_keys(
+        request_headers=dict(request.headers),
+        request_body=request.data,
+        request_method=request.method,
+        request_path=request.path,
+        databases=databases,
+    )
+    assert isinstance(database, VuforiaDatabase)
+    results = [target.target_id for target in database.not_deleted_targets]
+
+    body = {
+        'transaction_id': uuid.uuid4().hex,
+        'result_code': ResultCodes.SUCCESS.value,
+        'results': results,
+    }
+    date = email.utils.formatdate(None, localtime=False, usegmt=True)
+    headers = {
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'Server': 'nginx',
+        'Date': date,
+    }
+    return Response(
+        status=HTTPStatus.OK,
+        response=json_dump(body),
+        headers=headers,
+    )
+
+
+@VWS_FLASK_APP.route('/targets/<string:target_id>', methods=['PUT'])
+def update_target(target_id: str) -> Response:
+    """
+    Update a target.
+
+    Fake implementation of
+    https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API.html#How-To-Update-a-Target
+    """
+    # We do not use ``request.get_json(force=True)`` because this only works
+    # when the content type is given as ``application/json``.
+    request_json = json.loads(request.data)
+    databases = get_all_databases()
+    database = get_database_matching_server_keys(
+        request_headers=dict(request.headers),
+        request_body=request.data,
+        request_method=request.method,
+        request_path=request.path,
+        databases=databases,
+    )
+
+    assert isinstance(database, VuforiaDatabase)
+    [target] = [
+        target for target in database.targets if target.target_id == target_id
+    ]
+
+    if target.status != TargetStatuses.SUCCESS.value:
+        raise TargetStatusNotSuccess
+
+    update_values = {}
+    if 'width' in request_json:
+        update_values['width'] = request_json['width']
+
+    if 'active_flag' in request_json:
+        active_flag = request_json['active_flag']
+        if active_flag is None:
+            raise Fail(status_code=HTTPStatus.BAD_REQUEST)
+        update_values['active_flag'] = active_flag
+
+    if 'application_metadata' in request_json:
+        application_metadata = request_json['application_metadata']
+        if application_metadata is None:
+            raise Fail(status_code=HTTPStatus.BAD_REQUEST)
+        update_values['application_metadata'] = application_metadata
+
+    if 'name' in request_json:
+        name = request_json['name']
+        update_values['name'] = name
+
+    if 'image' in request_json:
+        image = request_json['image']
+        update_values['image'] = image
+
+    put_url = (
+        f'{STORAGE_BASE_URL}/databases/{database.database_name}/targets/'
+        f'{target_id}'
+    )
+    requests.put(url=put_url, json=update_values)
+
+    date = email.utils.formatdate(None, localtime=False, usegmt=True)
+    headers = {
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'Server': 'nginx',
+        'Date': date,
+    }
+    body = {
+        'result_code': ResultCodes.SUCCESS.value,
+        'transaction_id': uuid.uuid4().hex,
+    }
+    return Response(
+        status=HTTPStatus.OK,
+        response=json_dump(body),
+        headers=headers,
+    )
+
+if __name__ == '__main__':  # pragma: no cover
+    VWS_FLASK_APP.run(debug=True, host='0.0.0.0')
