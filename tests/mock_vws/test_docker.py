8b1340b72184ed87fae1b41121ed68cb3d30bfb9
Adam Dangoor <adamdangoor@gmail.com>
2020-09-22 08:34:00 +0100

    Make docker build work in test

--- tests/mock_vws/test_docker.py
+++ tests/mock_vws/test_docker.py
@@ -3,12 +3,17 @@ Tests for running the mock server in Docker.
 """
 
 import docker
+from pathlib import Path
 
 def test_build_and_run():
+    repository_root = Path(__file__).parent.parent.parent
     client = docker.from_env()
     # Build containers
-    dockerfile_path = ...
-    image, build_logs = client.images.build(path=dockerfile_path)
+    dockerfile_path = repository_root / 'src/mock_vws/_flask_server/Dockerfile'
+    image, build_logs = client.images.build(
+        path=str(repository_root),
+        dockerfile=str(dockerfile_path),
+    )
     container = client.containers.run(image=image, detach=True)
     # Run containers
 
