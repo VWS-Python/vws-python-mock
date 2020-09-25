9c4eaee1eb3d694fba4599dea2e9fab142ed909f
Adam Dangoor <adamdangoor@gmail.com>
2020-09-25 16:58:29 +0100

    Get test to the point of running a storage container

--- tests/mock_vws/test_docker.py
+++ tests/mock_vws/test_docker.py
@@ -9,27 +9,35 @@ def test_build_and_run():
     repository_root = Path(__file__).parent.parent.parent
     client = docker.from_env()
     # Build containers
-    dockerfile_dir = repository_root / 'src/mock_vws/_flask_server/Dockerfile'
+    dockerfile_dir = repository_root / 'src/mock_vws/_flask_server/dockerfiles'
 
-    base_dockerfile = dockerfile_dir / 'Dockerfile-base'
-    storage_dockerfile = dockerfile_dir / 'Dockerfile-storage'
-    vws_dockerfile = dockerfile_dir / 'Dockerfile-vws'
-    vwq_dockerfile = dockerfile_dir / 'Dockerfile-vwq'
+    base_dockerfile = dockerfile_dir / 'base/Dockerfile'
+    storage_dockerfile = dockerfile_dir / 'storage/Dockerfile'
+
+    base_tag = 'vws-mock:base'
+    base_image, build_logs = client.images.build(
+        path=str(repository_root),
+        dockerfile=str(base_dockerfile),
+        tag=base_tag,
+    )
+
+    # vws_dockerfile = dockerfile_dir / 'Dockerfile-vws'
+    # vwq_dockerfile = dockerfile_dir / 'Dockerfile-vwq'
     storage_image, build_logs = client.images.build(
         path=str(repository_root),
         dockerfile=str(storage_dockerfile),
     )
-    vws_image, build_logs = client.images.build(
-        path=str(repository_root),
-        dockerfile=str(vws_dockerfile),
-    )
-    vwq_image, build_logs = client.images.build(
-        path=str(repository_root),
-        dockerfile=str(vwq_dockerfile),
-    )
+    # vws_image, build_logs = client.images.build(
+    #     path=str(repository_root),
+    #     dockerfile=str(vws_dockerfile),
+    # )
+    # vwq_image, build_logs = client.images.build(
+    #     path=str(repository_root),
+    #     dockerfile=str(vwq_dockerfile),
+    # )
     storage_container = client.containers.run(image=storage_image, detach=True)
-    vws_container = client.containers.run(image=vws_image, detach=True)
-    vwq_container = client.containers.run(image=vwq_image, detach=True)
+    # vws_container = client.containers.run(image=vws_image, detach=True)
+    # vwq_container = client.containers.run(image=vwq_image, detach=True)
     # Run containers
 
     # Add target using vws_python
