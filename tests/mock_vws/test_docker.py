9c55be7f46f0c526e5b5fb7f92a9623a22441021
Adam Dangoor <adamdangoor@gmail.com>
2020-09-26 10:41:21 +0100

    Progress towards Docker images for each mock service

--- tests/mock_vws/test_docker.py
+++ tests/mock_vws/test_docker.py
@@ -4,15 +4,17 @@ Tests for running the mock server in Docker.
 
 import docker
 from pathlib import Path
+import requests
 
 def test_build_and_run():
     repository_root = Path(__file__).parent.parent.parent
     client = docker.from_env()
-    # Build containers
-    dockerfile_dir = repository_root / 'src/mock_vws/_flask_server/dockerfiles'
 
-    base_dockerfile = dockerfile_dir / 'base/Dockerfile'
-    storage_dockerfile = dockerfile_dir / 'storage/Dockerfile'
+    dockerfile_dir = repository_root / 'src/mock_vws/_flask_server/dockerfiles'
+    base_dockerfile = dockerfile_dir / 'base' / 'Dockerfile'
+    storage_dockerfile = dockerfile_dir / 'storage' / 'Dockerfile'
+    vws_dockerfile = dockerfile_dir / 'vws' / 'Dockerfile'
+    vwq_dockerfile = dockerfile_dir / 'vwq' / 'Dockerfile'
 
     base_tag = 'vws-mock:base'
     base_image, build_logs = client.images.build(
@@ -21,25 +23,22 @@ def test_build_and_run():
         tag=base_tag,
     )
 
-    # vws_dockerfile = dockerfile_dir / 'Dockerfile-vws'
-    # vwq_dockerfile = dockerfile_dir / 'Dockerfile-vwq'
     storage_image, build_logs = client.images.build(
         path=str(repository_root),
         dockerfile=str(storage_dockerfile),
     )
-    # vws_image, build_logs = client.images.build(
-    #     path=str(repository_root),
-    #     dockerfile=str(vws_dockerfile),
-    # )
-    # vwq_image, build_logs = client.images.build(
-    #     path=str(repository_root),
-    #     dockerfile=str(vwq_dockerfile),
-    # )
+    vws_image, build_logs = client.images.build(
+        path=str(repository_root),
+        dockerfile=str(vws_dockerfile),
+    )
+    vwq_image, build_logs = client.images.build(
+        path=str(repository_root),
+        dockerfile=str(vwq_dockerfile),
+    )
     storage_container = client.containers.run(
         image=storage_image,
         detach=True,
         publish_all_ports=True,
-        # ports={'5000/tcp': None},
     )
     storage_container.reload()
     import pdb; pdb.set_trace()
