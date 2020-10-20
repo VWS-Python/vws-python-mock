e29b60bc22fe6e24d194ed60ca12eadf37c95b7d
Adam Dangoor <adamdangoor@gmail.com>
2020-10-20 12:46:17 +0100

    Progress towards tests for usage of the Flask app

--- tests/mock_vws/test_flask_app_usage.py
+++ tests/mock_vws/test_flask_app_usage.py
@@ -7,12 +7,21 @@ class TestProcessingTime:
     Tests for the time taken to process targets in the mock.
     """
 
+class TestCustomQueryRecognizesDeletionSeconds:
+    """
+    Tests for setting the amount of time after a target has been deleted
+    until it is not recognized by the query endpoint.
+    """
+
 class TestDatabaseManagement:
     """
     TODO
     """
 
-    def test_add_database(self):
+    def test_duplicate_keys(self) -> None:
+        """
+        It is not possible to have multiple databases with matching keys.
+        """
         # Add one
         # Add another different
         # Add another conflict
