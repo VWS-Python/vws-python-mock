be6fa3c81f91972658992e06aae2eab2d8b0981d
Adam Dangoor <adamdangoor@gmail.com>
2020-10-20 10:07:26 +0100

    Remove use of global keyword

--- src/mock_vws/_flask_server/storage.py
+++ src/mock_vws/_flask_server/storage.py
@@ -31,13 +31,12 @@ def delete_database(database_name: str) -> Tuple[str, int]:
 
     :status 200: The database has been deleted.
     """
-    global VUFORIA_DATABASES
-    matching_databases = {
+    (matching_database,) = {
         database
         for database in VUFORIA_DATABASES
         if database_name == database.database_name
     }
-    VUFORIA_DATABASES = VUFORIA_DATABASES - matching_databases
+    VUFORIA_DATABASES.remove(matching_database)
     return '', HTTPStatus.OK
 
 
