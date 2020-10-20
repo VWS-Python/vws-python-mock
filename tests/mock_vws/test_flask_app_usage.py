4f54a2d2a55bd23428e012355047178233e0bc93
Adam Dangoor <adamdangoor@gmail.com>
2020-10-20 23:56:10 +0100

    Fix some lint issues

--- tests/mock_vws/test_flask_app_usage.py
+++ tests/mock_vws/test_flask_app_usage.py
@@ -2,40 +2,31 @@
 Tests for the usage of the mock Flask application.
 """
 
-from mock_vws._flask_server.target_manager import TARGET_MANAGER_FLASK_APP
-from mock_vws._flask_server.vwq import CLOUDRECO_FLASK_APP
-from mock_vws._flask_server.vws import VWS_FLASK_APP
-import requests
-import requests_mock
-from requests_mock_flask import add_flask_app_to_mock
 import io
-import email.utils
-import io
-import json
-import socket
 from datetime import datetime, timedelta
 
-import pytest
 import requests
-from freezegun import freeze_time
-from requests.exceptions import MissingSchema
-from requests_mock.exceptions import NoMockAddress
-from vws import VWS, CloudRecoService
-from vws.exceptions.cloud_reco_exceptions import MatchProcessing
+import requests_mock
+from _pytest.monkeypatch import MonkeyPatch
+from requests_mock_flask import add_flask_app_to_mock
+from vws import VWS
 from vws.reports import TargetStatuses
-from vws_auth_tools import rfc_1123_date
 
-from mock_vws import MockVWS
+from mock_vws._flask_server.target_manager import TARGET_MANAGER_FLASK_APP
+from mock_vws._flask_server.vws import VWS_FLASK_APP
 from mock_vws.database import VuforiaDatabase
-from mock_vws.states import States
-from mock_vws.target import Target
+
 
 class TestProcessingTime:
     """
     Tests for the time taken to process targets in the mock.
     """
 
-    def test_default(self, image_file_failed_state: io.BytesIO, monkeypatch) -> None:
+    def test_default(
+        self,
+        image_file_failed_state: io.BytesIO,
+        monkeypatch: MonkeyPatch,
+    ) -> None:
         """
         By default, targets in the mock take 0.5 seconds to be processed.
         """
@@ -62,7 +53,6 @@ class TestProcessingTime:
                 base_url=target_manager_base_url,
             )
 
-            # VWS_FLASK_APP.config['TARGET_MANAGER_BASE_URL'] = target_manager_base_url
             databases_url = target_manager_base_url + '/databases'
             requests.post(url=databases_url, json=database.to_dict())
 
@@ -89,12 +79,19 @@ class TestProcessingTime:
                     assert elapsed_time > timedelta(seconds=0.49)
                     return
 
-    def test_custom(self, image_file_failed_state: io.BytesIO) -> None:
+    def test_custom(
+        self,
+        image_file_failed_state: io.BytesIO,
+        monkeypatch: MonkeyPatch,
+    ) -> None:
         """
         It is possible to set a custom processing time.
         """
-        return
         target_manager_base_url = 'http://example.com'
+        monkeypatch.setenv(
+            name='TARGET_MANAGER_BASE_URL',
+            value=target_manager_base_url,
+        )
         database = VuforiaDatabase()
         vws_client = VWS(
             server_access_key=database.server_access_key,
@@ -113,7 +110,6 @@ class TestProcessingTime:
                 base_url=target_manager_base_url,
             )
 
-            VWS_FLASK_APP.config['TARGET_MANAGER_BASE_URL'] = target_manager_base_url
             mock.add_database(database=database)
             target_id = vws_client.add_target(
                 name='example',
@@ -137,17 +133,20 @@ class TestProcessingTime:
                     assert elapsed_time > timedelta(seconds=0.09)
                     return
 
+
 class TestCustomQueryRecognizesDeletionSeconds:
     """
     Tests for setting the amount of time after a target has been deleted
     until it is not recognized by the query endpoint.
     """
 
+
 class TestCustomQueryProcessDeletionSeconds:
     """
     Tests for setting the amount of time after a target has been deleted
     until it is not processed by the query endpoint.
     """
+
 
 class TestDatabaseManagement:
     """
@@ -161,7 +160,6 @@ class TestDatabaseManagement:
         # Add one
         # Add another different
         # Add another conflict
-        pass
 
     def test_give_no_details(self) -> None:
         # Random stuff
