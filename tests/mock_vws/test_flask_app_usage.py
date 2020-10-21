2a92c812db3e8c892eb6415bcc5b42c85ba54c05
Adam Dangoor <adamdangoor@gmail.com>
2020-10-21 09:52:23 +0100

    Progress towards tests for flask app usage

--- tests/mock_vws/test_flask_app_usage.py
+++ tests/mock_vws/test_flask_app_usage.py
@@ -8,16 +8,22 @@ from typing import Generator
 import uuid
 
 import requests
-import requests_mock
 from _pytest.monkeypatch import MonkeyPatch
 import pytest
 from requests_mock_flask import add_flask_app_to_mock
+from requests_mock import Mocker
 from vws import VWS
 from vws.reports import TargetStatuses
 
 from mock_vws._flask_server.target_manager import TARGET_MANAGER_FLASK_APP
 from mock_vws._flask_server.vws import VWS_FLASK_APP
 from mock_vws.database import VuforiaDatabase
+
+from tests.mock_vws.utils.usage_test_helpers import (
+    process_deletion_seconds,
+    recognize_deletion_seconds,
+    processing_time_seconds,
+)
 
 
 @pytest.fixture()
@@ -28,31 +34,34 @@ def target_manager_base_url() -> str:
 def enable_requests_mock(
     target_manager_base_url: str,
     monkeypatch: MonkeyPatch,
-) -> Generator:
-    with requests_mock.Mocker(real_http=False) as mock:
-        add_flask_app_to_mock(
-            mock_obj=mock,
-            flask_app=VWS_FLASK_APP,
-            base_url='https://vws.vuforia.com',
-        )
+    requests_mock: Mocker,
+) -> None:
+    add_flask_app_to_mock(
+        mock_obj=requests_mock,
+        flask_app=VWS_FLASK_APP,
+        base_url='https://vws.vuforia.com',
+    )
 
-        add_flask_app_to_mock(
-            mock_obj=mock,
-            flask_app=TARGET_MANAGER_FLASK_APP,
-            base_url=target_manager_base_url,
-        )
+    add_flask_app_to_mock(
+        mock_obj=requests_mock,
+        flask_app=TARGET_MANAGER_FLASK_APP,
+        base_url=target_manager_base_url,
+    )
 
-        monkeypatch.setenv(
-            name='TARGET_MANAGER_BASE_URL',
-            value=target_manager_base_url,
-        )
+    monkeypatch.setenv(
+        name='TARGET_MANAGER_BASE_URL',
+        value=target_manager_base_url,
+    )
 
-        yield
 
 class TestProcessingTime:
     """
     Tests for the time taken to process targets in the mock.
     """
+
+    # There is a race condition in this test type - if tests start to
+    # fail, consider increasing the leeway.
+    LEEWAY = 0.05
 
     def test_default(
         self,
@@ -72,28 +81,13 @@ class TestProcessingTime:
         databases_url = target_manager_base_url + '/databases'
         requests.post(url=databases_url, json=database.to_dict())
 
-        target_id = vws_client.add_target(
-            name='example',
-            width=1,
+        time_taken = processing_time_seconds(
+            vuforia_database=database,
             image=image_file_failed_state,
-            active_flag=True,
-            application_metadata=None,
         )
-        start_time = datetime.now()
 
-        while True:
-            target_details = vws_client.get_target_record(
-                target_id=target_id,
-            )
-
-            status = target_details.status
-            if status != TargetStatuses.PROCESSING:
-                elapsed_time = datetime.now() - start_time
-                # There is a race condition in this test - if it starts to
-                # fail, maybe extend the acceptable range.
-                assert elapsed_time < timedelta(seconds=0.55)
-                assert elapsed_time > timedelta(seconds=0.49)
-                return
+        expected = 0.5
+        assert abs(expected - time_taken) < self.LEEWAY
 
     def test_custom(
         self,
@@ -116,27 +110,14 @@ class TestProcessingTime:
 
         databases_url = target_manager_base_url + '/databases'
         requests.post(url=databases_url, json=database.to_dict())
-        target_id = vws_client.add_target(
-            name='example',
-            width=1,
+
+        time_taken = processing_time_seconds(
+            vuforia_database=database,
             image=image_file_failed_state,
-            active_flag=True,
-            application_metadata=None,
         )
 
-        start_time = datetime.now()
-
-        while True:
-            target_details = vws_client.get_target_record(
-                target_id=target_id,
-            )
-
-            status = target_details.status
-            if status != TargetStatuses.PROCESSING:
-                elapsed_time = datetime.now() - start_time
-                assert elapsed_time < timedelta(seconds=0.15)
-                assert elapsed_time > timedelta(seconds=0.09)
-                return
+        expected = 0.1
+        assert abs(expected - time_taken) < self.LEEWAY
 
 
 class TestCustomQueryRecognizesDeletionSeconds:
@@ -144,34 +125,64 @@ class TestCustomQueryRecognizesDeletionSeconds:
     Tests for setting the amount of time after a target has been deleted
     until it is not recognized by the query endpoint.
     """
-    def _process_deletion_seconds(
+
+    LEEWAY = 0.15
+
+    def test_default(
         self,
         high_quality_image: io.BytesIO,
-        vuforia_database: VuforiaDatabase,
-    ) -> float:
+        target_manager_base_url: str,
+    ) -> None:
         """
-        The number of seconds it takes for the query endpoint to process a
-        deletion.
+        By default it takes zero seconds for the Query API on the mock to
+        recognize that a target has been deleted.
+
+        The real Query API takes between zero and two seconds.
+        See ``test_query`` for more information.
         """
-        _add_and_delete_target(
-            image=high_quality_image,
-            vuforia_database=vuforia_database,
+        database = VuforiaDatabase()
+        databases_url = target_manager_base_url + '/databases'
+        requests.post(url=databases_url, json=database.to_dict())
+        time_taken = recognize_deletion_seconds(
+            high_quality_image=high_quality_image,
+            vuforia_database=database,
         )
 
-        _wait_for_deletion_recognized(
-            image=high_quality_image,
-            vuforia_database=vuforia_database,
+        expected = 0.2
+        assert abs(expected - time_taken) < self.LEEWAY
+
+    def test_custom(
+        self,
+        high_quality_image: io.BytesIO,
+        monkeypatch: MonkeyPatch,
+    ) -> None:
+        """
+        It is possible to use set a custom amount of time that it takes for the
+        Query API on the mock to recognize that a target has been deleted.
+        """
+        # We choose a low time for a quick test.
+        query_recognizes_deletion = 0.5
+        database = VuforiaDatabase()
+        databases_url = target_manager_base_url + '/databases'
+        requests.post(url=databases_url, json=database.to_dict())
+        monkeypatch.setenv(
+            name='TARGET_MANAGER_BASE_URL',
+            value=query_recognizes_deletion,
+        )
+        time_taken = recognize_deletion_seconds(
+            high_quality_image=high_quality_image,
+            vuforia_database=database,
         )
 
-        time_after_deletion_recognized = datetime.now()
+        expected = query_recognizes_deletion
+        assert abs(expected - time_taken) < self.LEEWAY
 
-        _wait_for_deletion_processed(
-            image=high_quality_image,
-            vuforia_database=vuforia_database,
-        )
 
-        time_difference = datetime.now() - time_after_deletion_recognized
-        return time_difference.total_seconds()
+class TestCustomQueryProcessDeletionSeconds:
+    """
+    Tests for setting the amount of time after a target has been deleted
+    until it is not processed by the query endpoint.
+    """
 
     def test_default(
         self,
@@ -187,13 +198,13 @@ class TestCustomQueryRecognizesDeletionSeconds:
         database = VuforiaDatabase()
         with MockVWS() as mock:
             mock.add_database(database=database)
-            process_deletion_seconds = self._process_deletion_seconds(
+            time_taken = process_deletion_seconds(
                 high_quality_image=high_quality_image,
                 vuforia_database=database,
             )
 
         expected = 3
-        assert abs(expected - process_deletion_seconds) < 0.1
+        assert abs(expected - time_taken) < 0.1
 
     def test_custom(
         self,
@@ -210,20 +221,13 @@ class TestCustomQueryRecognizesDeletionSeconds:
             query_processes_deletion_seconds=query_processes_deletion,
         ) as mock:
             mock.add_database(database=database)
-            process_deletion_seconds = self._process_deletion_seconds(
+            time_taken = process_deletion_seconds(
                 high_quality_image=high_quality_image,
                 vuforia_database=database,
             )
 
         expected = query_processes_deletion
-        assert abs(expected - process_deletion_seconds) < 0.1
-
-
-class TestCustomQueryProcessDeletionSeconds:
-    """
-    Tests for setting the amount of time after a target has been deleted
-    until it is not processed by the query endpoint.
-    """
+        assert abs(expected - time_taken) < 0.1
 
 
 class TestDatabaseManagement:
--- tests/mock_vws/test_requests_mock_usage.py
+++ tests/mock_vws/test_requests_mock_usage.py
@@ -241,7 +241,7 @@ class TestCustomQueryRecognizesDeletionSeconds:
         expected = 0.2
         assert abs(expected - time_taken) < self.LEEWAY
 
-    def test_with_noprocessing_time(
+    def test_with_no_processing_time(
         self,
         high_quality_image: io.BytesIO,
     ) -> None:
