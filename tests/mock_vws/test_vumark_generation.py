4f43823fcd53ef5071838b70f249fd7eea91ee09
Adam Dangoor <adamdangoor@gmail.com>
2024-03-01 14:35:46 +0000

    Expand stubs for Vumark

--- tests/mock_vws/test_vumark_generation.py
+++ tests/mock_vws/test_vumark_generation.py
@@ -7,13 +7,19 @@ class TestVuMarkInstanceGeneration:
     Tests for generating VuMark instances.
     """
 
+    # Content type: svg+xml, image/png, application/pdf
     def test_generate_vumark_instance(self) -> None:
-        pass
+        target_id = "..."
+        url = "https://vws.vuforia.com/targets/{target_id}/instances"
+        # TODO: Fill this in
 
     def test_target_does_not_exist(self) -> None:
-        pass
+        url = "https://vws.vuforia.com/targets/{target_id}/instances"
 
-    def test_instance_id_does_not_exist(self) -> None:
+    def test_invalid_instance_id(self) -> None:
+        # Negative, too large, float, illegal characters
+        # too many hex characters
+        # string too long
         pass
 
     def test_target_status_is_processing(self) -> None:
