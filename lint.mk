7a506c4e30791fb6cc2605e568dc2c08b6d39fab
Adam Dangoor <adamdangoor@gmail.com>
2024-05-15 23:02:27 +0100

    Move pyproject-fmt to pre-commit

--- lint.mk
+++ lint.mk
@@ -6,14 +6,6 @@ SHELL := /bin/bash -euxo pipefail
 linkcheck:
 	$(MAKE) -C docs/ linkcheck SPHINXOPTS=$(SPHINXOPTS)
 
-.PHONY: pyproject-fmt
-pyproject-fmt:
-	pyproject-fmt --check pyproject.toml
-
-.PHONY: fix-pyproject-fmt
-fix-pyproject-fmt:
-	pyproject-fmt pyproject.toml
-
 .PHONY: spelling
 spelling:
 	$(MAKE) -C docs/ spelling SPHINXOPTS=$(SPHINXOPTS)
