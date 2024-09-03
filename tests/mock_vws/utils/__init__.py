7f24e069f647690247ebc971dd8101231276e622
Adam Dangoor <adamdangoor@gmail.com>
2024-09-03 22:45:33 +0100

    Make Endpoint a dataclass - simpler, and easier to change upcoming

--- tests/mock_vws/utils/__init__.py
+++ tests/mock_vws/utils/__init__.py
@@ -4,6 +4,7 @@ Utilities for tests.
 
 import io
 import secrets
+from dataclasses import dataclass
 from typing import Literal
 
 import requests
@@ -12,49 +13,44 @@ from PIL import Image
 from mock_vws._constants import ResultCodes
 
 
+@dataclass
 class Endpoint:
     """
     Details of endpoints to be called in tests.
+
+    Args:
+        prepared_request: A request to make which would be successful.
+        successful_headers_result_code: The expected result code if the
+            example path is requested with the method.
+        successful_headers_status_code: The expected status code if the
+            example path is requested with the method.
+        access_key: The access key used in the prepared request.
+        secret_key: The secret key used in the prepared request.
+
+    Attributes:
+        prepared_request: A request to make which would be successful.
+        successful_headers_result_code: The expected result code if the
+            example path is requested with the method.
+        successful_headers_status_code: The expected status code if the
+            example path is requested with the method.
+        access_key: The access key used in the prepared request.
+        secret_key: The secret key used in the prepared request.
     """
 
-    def __init__(
-        self,
-        prepared_request: requests.PreparedRequest,
-        successful_headers_result_code: ResultCodes,
-        successful_headers_status_code: int,
-        access_key: str,
-        secret_key: str,
-    ) -> None:
-        """
-        Args:
-            prepared_request: A request to make which would be successful.
-            successful_headers_result_code: The expected result code if the
-                example path is requested with the method.
-            successful_headers_status_code: The expected status code if the
-                example path is requested with the method.
-            access_key: The access key used in the prepared request.
-            secret_key: The secret key used in the prepared request.
+    prepared_request: requests.PreparedRequest
+    successful_headers_result_code: ResultCodes
+    successful_headers_status_code: int
+    access_key: str
+    secret_key: str
 
-        Attributes:
-            prepared_request: A request to make which would be successful.
-            successful_headers_result_code: The expected result code if the
-                example path is requested with the method.
-            successful_headers_status_code: The expected status code if the
-                example path is requested with the method.
-            auth_header_content_type: The content type to use for the
-                `Authorization` header.
-            access_key: The access key used in the prepared request.
-            secret_key: The secret key used in the prepared request.
+    @property
+    def auth_header_content_type(self) -> str:
         """
-        self.prepared_request = prepared_request
-        self.successful_headers_status_code = successful_headers_status_code
-        self.successful_headers_result_code = successful_headers_result_code
-        headers = prepared_request.headers
-        content_type = headers.get("Content-Type", "")
-        content_type = content_type.split(sep=";")[0]
-        self.auth_header_content_type: str = content_type
-        self.access_key = access_key
-        self.secret_key = secret_key
+        The content type to use for the `Authorization` header.
+        """
+        headers = self.prepared_request.headers
+        full_content_type = headers.get("Content-Type", "")
+        return full_content_type.split(sep=";")[0]
 
 
 def make_image_file(
