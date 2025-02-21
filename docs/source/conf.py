00ef1ff67bd887d7b595841bf5edde7e4af96c84
Adam Dangoor <adamdangoor@gmail.com>
2025-02-21 12:24:25 +0000

    Remove double setting of autoclass content - just use both

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ pygments_style = "sphinx"
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "VWSPYTHONMOCKdoc"
-autoclass_content = "init"
+autoclass_content = "both"
 intersphinx_mapping = {
     "python": (f"https://docs.python.org/{minimum_python_version}", None),
     "docker": ("https://docker-py.readthedocs.io/en/stable", None),
