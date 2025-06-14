eb32bec82ab5f7ee7c2beffee33e95b408146c6d
Adam Dangoor <adamdangoor@gmail.com>
2025-06-14 22:03:23 +0100

    Use pytest-beartype

--- conftest.py
+++ conftest.py
@@ -14,17 +14,6 @@ from sybil.parsers.rest import (
 
 from tests.mock_vws.utils.retries import RETRY_EXCEPTIONS
 
-
-@beartype
-def pytest_collection_modifyitems(items: list[pytest.Item]) -> None:
-    """
-    Apply the beartype decorator to all collected test functions.
-    """
-    for item in items:
-        if isinstance(item, pytest.Function):
-            item.obj = beartype(obj=item.obj)
-
-
 pytest_collect_file = Sybil(
     parsers=[
         DocTestParser(optionflags=ELLIPSIS),
