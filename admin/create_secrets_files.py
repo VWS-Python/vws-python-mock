0850aa718e416cd9d1a993302606ae3ce34af42a
Adam Dangoor <adamdangoor@gmail.com>
2025-10-27 22:29:07 +0000

    Improve docstring

--- admin/create_secrets_files.py
+++ admin/create_secrets_files.py
@@ -4,14 +4,12 @@ Usage:
 
     $ export VWS_EMAIL_ADDRESS=...
     $ export VWS_PASSWORD=...
-    # For ``make update-secrets`` to work, this has to be ``./ci_secrets``, or
-    # you have to copy the secrets there later.
     $ export NEW_SECRETS_DIR=...
     $ export EXISTING_SECRETS_FILE=/existing/file/with/inactive/db/creds
     # You may have to run this a few times, but it is idempotent.
     $ python admin/create_secrets_files.py
     # After creating the secrets, update the encrypted archive:
-    $ tar cvf secrets.tar ci_secrets/
+    $ tar cvf secrets.tar "${NEW_SECRETS_DIR}"
     $ gpg \
         --yes \
         --batch \
